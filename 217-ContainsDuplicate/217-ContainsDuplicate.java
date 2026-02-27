e569c01b606e10966c7032416cc15986dffaef47
Jasprit Grewal <153491291+sinferna@users.noreply.github.com>
2026-02-26 21:47:36 -0700

    Updated ContainsDuplicate to refactored solution

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,12 +1,12 @@
 // Last updated: 2/26/2026, 9:30:19 PM
 1class Solution {
 2    public boolean containsDuplicate(int[] nums) {
-3        HashMap<Integer, Integer> seen = new HashMap<>();
+3        HashSet<Integer> seen = new HashSet<>();
 4            for (int i = 0; i < nums.length; i++) {
-5                if (seen.containsKey(nums[i])) {
+5                if (seen.contains(nums[i])) {
 6                    return true;
 7                }
-8                seen.put(nums[i], i);
+8                seen.add(nums[i]);
 9            }
 10            return false;
 11    }
